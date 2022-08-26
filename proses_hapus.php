de8c25b67e5ae793fa1d9d75f80a8b47722d0281
fiaui <109265532+fiaui@users.noreply.github.com>
2022-08-26 08:24:42 +0700

    Delete proses_hapus.php

--- proses_hapus.php
+++
@@ -1,9 +0,0 @@
-<?php
-$id = $_GET['id'];
-
-$db = new PDO("mysql:host=localhost;dbname=sekolah3", 'root', '');
-$query = $db->query("DELETE FROM siswa WHERE id='$id'");
-
-if($query){
-    header("location:index.php");
-}
